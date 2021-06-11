0c202ab2081f497a9959e5fba64bfad9be65eb8d
daiki_U <daikiumehara3@gmail.com>
2021-06-12 00:47:06 +0900

    セクション4が終了しました

--- UIKit_Practice_01/TaskCell.swift
+++ UIKit_Practice_01/TaskCell.swift
@@ -9,16 +9,23 @@ import UIKit
 
 class TaskCell: UITableViewCell {
 
+    typealias DoneButtonAction = () -> Void
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
     @IBOutlet var dateLabel: UILabel!
+
+    var doneButtonAction: DoneButtonAction?
 
     static var identifier: String { String(describing: self) }
     static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
 
+    @IBAction func didTapDoneButton(_ sender: Any) {
+        doneButtonAction?()
+    }
 }
 
 extension TaskCell {
     func setup(title: String, date: Date) {
-        
+
     }
 }
