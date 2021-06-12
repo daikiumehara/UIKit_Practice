89364680e42fdd006249759732461ac2cf194171
daiki_U <daikiumehara3@gmail.com>
2021-06-12 13:20:18 +0900

    セクション５が終了しました

--- UIKit_Practice_01/ReminderListViewController.swift
+++ UIKit_Practice_01/ReminderListViewController.swift
@@ -7,35 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ReminderListViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(TaskCell.nib, forCellReuseIdentifier: TaskCell.identifier)
     }
+}
 
+extension ReminderListViewController: UITableViewDelegate {
 
 }
 
-extension ViewController: UITableViewDelegate {
-
-}
-
-extension ViewController: UITableViewDataSource {
+extension ReminderListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return Reminder.testData.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: TaskCell.identifier, for: indexPath) as! TaskCell
+        let task = Reminder.testData[indexPath.row]
+        cell.setup(task: task) { [weak self] in
+            guard let self = self else { return }
+            Reminder.testData[indexPath.row].isComplete.toggle()
+            self.tableView.reloadData()
+        }
         return cell
     }
 
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        60.0
+        100.0
     }
 }
 
--- UIKit_Practice_01/TaskCell.swift
+++ UIKit_Practice_01/TaskCell.swift
@@ -2,19 +2,18 @@
 //  TaskCell.swift
 //  UIKit_Practice_01
 //
-//  Created by daiki umehara on 2021/06/11.
+//  Created by daiki umehara on 2021/06/12.
 //
 
 import UIKit
 
 class TaskCell: UITableViewCell {
-
     typealias DoneButtonAction = () -> Void
-    @IBOutlet var titleLabel: UILabel!
     @IBOutlet var doneButton: UIButton!
+    @IBOutlet var titleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
 
-    var doneButtonAction: DoneButtonAction?
+    private var doneButtonAction: DoneButtonAction?
 
     static var identifier: String { String(describing: self) }
     static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
@@ -25,7 +24,12 @@ class TaskCell: UITableViewCell {
 }
 
 extension TaskCell {
-    func setup(title: String, date: Date) {
-
+    func setup(task: Reminder, buttonAction: @escaping DoneButtonAction) {
+        titleLabel.text = task.title
+        dateLabel.text = task.dueDate.description
+        doneButton.setBackgroundImage(task.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle"),
+                                      for: .normal)
+        doneButtonAction = buttonAction
     }
+    
 }
